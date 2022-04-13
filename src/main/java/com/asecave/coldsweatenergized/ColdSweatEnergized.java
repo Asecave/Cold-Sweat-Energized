b22abd6439c9ed992be953efed780ae402a9a5f3
Asecave <39377667+Asecave@users.noreply.github.com>
2022-04-14 01:29:36 +0200

    added crafting armor

--- src/main/java/com/asecave/coldsweatenergized/ColdSweatEnergized.java
+++ src/main/java/com/asecave/coldsweatenergized/ColdSweatEnergized.java
@@ -1,23 +1,26 @@
-package com.asecave;
+package com.asecave.coldsweatenergized;
 
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.asecave.coldsweatenergized.item.Items;
+
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.momostudios.coldsweat.ColdSweat;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ColdSweatEnergized.MOD_ID)
 public class ColdSweatEnergized {
 
     public static final Logger LOGGER = LogManager.getLogger();
-	public static final String MOD_ID = "cold_sweat_energized";
+	public static final String MOD_ID = "coldsweatenergized";
 	
 	public ColdSweatEnergized() {
 		
 		MinecraftForge.EVENT_BUS.register(this);
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 		
-		LOGGER.log(Level.INFO, ColdSweat.MOD_ID);
+		Items.register(bus);
 	}
 }
---
+++ src/main/java/com/asecave/coldsweatenergized/item/Items.java
@@ -0,0 +1,24 @@
+package com.asecave.coldsweatenergized.item;
+
+import com.asecave.coldsweatenergized.ColdSweatEnergized;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.momostudios.coldsweat.core.itemgroup.ColdSweatGroup;
+
+public class Items {
+
+	public final static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ColdSweatEnergized.MOD_ID);
+	
+	public static final RegistryObject<Item> HELMET = ITEMS.register("helmet", () -> new Item(new Item.Properties().group(ColdSweatGroup.COLD_SWEAT)));
+	public static final RegistryObject<Item> CHESTPLATE = ITEMS.register("chestplate", () -> new Item(new Item.Properties().group(ColdSweatGroup.COLD_SWEAT)));
+	public static final RegistryObject<Item> LEGGINS = ITEMS.register("leggins", () -> new Item(new Item.Properties().group(ColdSweatGroup.COLD_SWEAT)));
+	public static final RegistryObject<Item> BOOTS = ITEMS.register("boots", () -> new Item(new Item.Properties().group(ColdSweatGroup.COLD_SWEAT)));
+	
+	public static void register(IEventBus bus) {
+		ITEMS.register(bus);
+	}
+}
